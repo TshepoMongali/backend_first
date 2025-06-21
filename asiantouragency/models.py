d85e07d86828d85cb4d5d273894769412b6ca170
kennymongali <s.mongali@gmail.com>
2025-06-22 01:21:40 +0200

    made changes ont he database

--- asiantouragency/models.py
+++ asiantouragency/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Tour(models.Model):
+    # origin country
+    origin_country = models.CharField(max_length=64)
+    # destination
+    destination_country = models.CharField(max_length=64)
+    # number of nights
+    number_of_nights = models.IntegerField()
+    
+    # prices for the tour
+    
+    price = models.IntegerField()
+    
+    #string epresentation of tours
+    
+    def __str__(self):
+        return (f"ID:{self.id}:From {self.origin_country} To {self.destination_country},{self.number_of_nights}nights costs $ {self.price}")
